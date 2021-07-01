a6bb8a580addb46551616814e81d14cb1961c582
Ian Draves <iantdraves@gmail.com>
2021-07-01 00:24:24 +0000

    Clarifying comments

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from random import *
+from random import randint
 
 # Vars
 trial_results_scen1 = []
@@ -9,50 +9,50 @@ scenario_1_results = []
 scenario_2_results = []
 number_of_trials = 1000000
 
-# Main function
 def main():
-    # Scenario 1 (outsider)
-    for i in range(0, number_of_trials):
-        trial = randint(0, 1)
-        
-        if trial == 0:
-            trial_results_scen1.append(0)
-        else:
-            intra_trial = randint(0, 1)
+	print("Guess Accuracy % \n")
 
-            if intra_trial == 0:
-                trial_results_scen1.append(1)
-            else:
-                trial_results_scen1.append(2)
+	# Scenario 1 (exclusive)
+	for i in range(0, number_of_trials):
+		trial = randint(0, 1)
+
+		if trial == 0:
+			trial_results_scen1.append(0)
+		else:
+			intra_trial = randint(0, 1)
+			if intra_trial == 0:
+				trial_results_scen1.append(1)
+			else:
+				trial_results_scen1.append(2)
 
 
-        guess = randint(0, 2)
-        trial_guesses_scen1.append(guess)
-    
-    for i in range(0, (len(trial_results_scen1) - 1)):
-        if trial_guesses_scen1[i] == trial_results_scen1[i]:
-            scenario_1_results.append(trial_guesses_scen1[i])
+		guess = randint(0, 2)
+		trial_guesses_scen1.append(guess)
 
-    print("Scenario 1: " + str(round((len(scenario_1_results) / len(trial_results_scen1)), 2)))
+	for i in range(0, (len(trial_results_scen1) - 1)):
+		if trial_guesses_scen1[i] == trial_results_scen1[i]:
+			scenario_1_results.append(trial_guesses_scen1[i])
 
-    # Scenario 2 (insider)
-    for i in range(0, number_of_trials):
-        trial = randint(0, 1)
+	print("Exclusive Test: " + str(100 * round((len(scenario_1_results) / len(trial_results_scen1)), 2)))
 
-        if trial == 0:
-            trial_results_scen2.append(0)
-            trial_guesses_scen2.append(randint(0, 2))
-        else:
-            trial_results_scen2.append(1)
-            trial_guesses_scen2.append(randint(0, 2))
-            trial_results_scen2.append(2)
-            trial_guesses_scen2.append(randint(0, 2))
+	# Scenario 2 (inclusive)
+	for i in range(0, number_of_trials):
+		trial = randint(0, 1)
 
-    for i in range(0, (len(trial_results_scen2) - 1)):
-        if trial_guesses_scen2[i] == trial_results_scen2[i]:
-            scenario_2_results.append(trial_guesses_scen2[i])
+		if trial == 0:
+			trial_results_scen2.append(0)
+			trial_guesses_scen2.append(randint(0, 2))
+		else:
+			trial_results_scen2.append(1)
+			trial_guesses_scen2.append(randint(0, 2))
+			trial_results_scen2.append(2)
+			trial_guesses_scen2.append(randint(0, 2))
 
-    print("Scenario 2: " + str(round((len(scenario_2_results) / len(trial_results_scen2)), 2)))
+	for i in range(0, (len(trial_results_scen2) - 1)):
+		if trial_guesses_scen2[i] == trial_results_scen2[i]:
+			scenario_2_results.append(trial_guesses_scen2[i])
+
+	print("Inclusive Test: " + str(100 * round((len(scenario_2_results) / len(trial_results_scen2)), 2)))
 
 if __name__ == '__main__':
-    main()
+	main()
